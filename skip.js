e38020459279dfcfe557c4b45e9ffd5e9d6e2f84
Nikhil Singh <30321610+ImSnik@users.noreply.github.com>
2018-12-17 09:03:34 +0530

    Final update

--- skip.js
+++ skip.js
@@ -2,7 +2,4 @@
     if(document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text").length === 1){
         document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text")[0].click();
     }
-    else {
-        console.log('');
-    }
-},750);
+},1);
